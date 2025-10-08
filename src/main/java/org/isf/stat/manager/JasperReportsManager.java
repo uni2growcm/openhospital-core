2cb81d64a07d7b733e1bb02c9a8b80db1f051d33
serene21 <hemaserenankemenyi@gmail.com>
2025-10-08 16:55:05 +0100

    add pharmaceuticalAMC report in RPT base

--- src/main/java/org/isf/stat/manager/JasperReportsManager.java
+++ src/main/java/org/isf/stat/manager/JasperReportsManager.java
@@ -462,6 +462,32 @@ public class JasperReportsManager {
 		}
 	}
 
+	public JasperReportResultDto GenericReportPharmaceuticalAMCPdf(LocalDateTime date, String jasperFileName, Locale locale) throws OHServiceException {
+		try {
+			if (date == null) {
+				date = TimeTools.getNow();
+			}
+			HashMap<String, Object> parameters = compileGenericReportPharmaceuticalAMCparameters(date);
+
+			if (locale != null) {
+				parameters.put(JRParameter.REPORT_LOCALE, locale);
+			}
+
+			addBundleParameter(RPT_BASE, jasperFileName, parameters);
+
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern(YYYY_MM_DD);
+			String todayFile = formatter.format(date);
+			String pdfFilename = compilePDFFilename(RPT_BASE, jasperFileName, Arrays.asList(todayFile), "pdf");
+
+			JasperReportResultDto result = generateJasperReport(compileJasperFilename(RPT_BASE, jasperFileName), pdfFilename, parameters);
+			JasperExportManager.exportReportToPdfFile(result.getJasperPrint(), pdfFilename);
+			return result;
+		} catch (Exception e) {
+			LOGGER.error("", e);
+			throw new OHReportException(e, new OHExceptionMessage(MessageBundle.getMessage(STAT_REPORTERROR_MSG)));
+		}
+	}
+
 	public void getGenericReportPharmaceuticalAMCExcel(LocalDateTime date, String jasperFileName, String exportFilename) throws OHServiceException {
 		try {
 			if (date == null) {
@@ -494,6 +520,52 @@ public class JasperReportsManager {
 			}
 		} catch (Exception e) {
 			LOGGER.error("", e);
+			throw new OHReportException(e, new OHExceptionMessage(MessageBundle.getMessage(STAT_REPORTERROR_MSG)));
+		}
+	}
+
+	public void getGenericReportPharmaceuticalAMCExcel(LocalDateTime date, String jasperFileName, String exportFilename, Locale locale)
+		throws OHServiceException {
+		try {
+			if (date == null) {
+				date = TimeTools.getNow();
+			}
+
+			HashMap<String, Object> parameters = compileGenericReportPharmaceuticalAMCparameters(date);
+
+			if (locale != null) {
+				parameters.put(JRParameter.REPORT_LOCALE, locale);
+			}
+
+			String dateTodayQuery = TimeTools.formatDateTime((LocalDateTime) parameters.get("TODAY_DATE"), YYYY_MM_DD);
+			String dateStartQuery = TimeTools.formatDateTime((LocalDateTime) parameters.get("START_DATE"), YYYY_MM_DD);
+			String dateEndQuery = TimeTools.formatDateTime((LocalDateTime) parameters.get("END_DATE"), YYYY_MM_DD);
+
+			File jasperFile = new File(compileJasperFilename(RPT_BASE, jasperFileName));
+			JasperReport jasperReport = (JasperReport) JRLoader.loadObject(jasperFile);
+
+			JRQuery query = jasperReport.getMainDataset().getQuery();
+			String queryString = query.getText()
+				.replace("$P{TODAY_DATE}", "'" + dateTodayQuery + "'")
+				.replace("$P{START_DATE}", "'" + dateStartQuery + "'")
+				.replace("$P{END_DATE}", "'" + dateEndQuery + "'");
+
+			DbQueryLogger dbQuery = new DbQueryLogger();
+			ResultSet resultSet = dbQuery.getData(queryString, true);
+
+			File exportFile = new File(exportFilename);
+			ExcelExporter xlsExport = new ExcelExporter();
+
+			if (exportFile.getName().endsWith(".xls")) {
+				xlsExport.exportResultsetToExcelOLD(resultSet, exportFile);
+			} else {
+				xlsExport.exportResultsetToExcel(resultSet, exportFile);
+			}
+
+			LOGGER.info("Generated Pharmaceutical AMC Excel report: {}", exportFile.getAbsolutePath());
+
+		} catch (Exception e) {
+			LOGGER.error("Error generating Pharmaceutical AMC Excel report", e);
 			throw new OHReportException(e, new OHExceptionMessage(MessageBundle.getMessage(STAT_REPORTERROR_MSG)));
 		}
 	}
