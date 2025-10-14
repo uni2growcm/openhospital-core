49927666e93734d277b76534ede3800d244e9ee5
SteveGT96 <steve.tsala@intesys.it>
2025-10-14 11:17:17 +0100

    update(OH2-160): Add medical lots property to model

--- src/main/java/org/isf/medicalstock/model/Lot.java
+++ src/main/java/org/isf/medicalstock/model/Lot.java
@@ -25,9 +25,11 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -59,7 +61,7 @@ public class Lot extends Auditable<String> {
 	private String code;
 
 	@NotNull
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY, cascade = { CascadeType.REMOVE })
 	@JoinColumn(name="LT_MDSR_ID")
 	private Medical medical;
 
