036029342bce3f4dbb8c572be16ed0bc316411c2
Aurorj <aurellejoellen@gmail.com>
2026-04-23 16:03:30 +0100

    update script name

--- sql/step_121_add_maternity_menuitem_and_privilege_for_groupmenu.sql
+++ sql/step_121_add_maternity_menuitem_and_privilege_for_groupmenu.sql
@@ -1,12 +1,12 @@
--- Add pregnancycare menuitem and privilege (if not exists)
+-- Add maternity menuitem and privilege (if not exists)
 
 INSERT INTO `oh_menuitem` (`MNI_ID_A`, `MNI_BTN_LABEL`, `MNI_LABEL`, `MNI_TOOLTIP`, `MNI_SHORTCUT`, `MNI_SUBMENU`, `MNI_CLASS`, `MNI_IS_SUBMENU`, `MNI_POSITION`)
-SELECT 'pregnancycare','angal.menu.btn.pregnancycare','angal.menu.pregnancycare','x','E','main','org.isf.pregnancycare.gui.PregnancyCareBrowser','N',5
+SELECT 'maternity','angal.menu.btn.maternity','angal.menu.maternity','x','M','main','org.isf.maternity.gui.MaternityBrowser','N',5
 FROM DUAL
-WHERE NOT EXISTS (SELECT 1 FROM `oh_menuitem` WHERE `MNI_ID_A` = 'pregnancycare');
+WHERE NOT EXISTS (SELECT 1 FROM `oh_menuitem` WHERE `MNI_ID_A` = 'maternity');
 
 INSERT INTO `oh_groupmenu` (`GM_ID`, `GM_UG_ID_A`, `GM_MNI_ID_A`, `GM_ACTIVE`, `GM_CREATED_BY`, `GM_CREATED_DATE`, `GM_LAST_MODIFIED_BY`, `GM_LAST_MODIFIED_DATE`)
-SELECT 351, 'admin', 'pregnancycare', 1, NULL, NULL, NULL, NULL
+SELECT 351, 'admin', 'maternity', 1, NULL, NULL, NULL, NULL
 FROM DUAL
 WHERE NOT EXISTS (SELECT 1 FROM `oh_groupmenu` WHERE `GM_ID` = 351);
 
