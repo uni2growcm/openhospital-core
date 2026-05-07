0daf8ff7ecf9efbced21713a563e069fb7064449
rivald123 <stephannintedem@gmail.com>
2026-05-07 14:36:41 +0100

    add license of project

--- src/main/java/org/isf/country/service/CountryIoOperationRepository.java
+++ src/main/java/org/isf/country/service/CountryIoOperationRepository.java
@@ -1,3 +1,25 @@
+/*
+ * Open Hospital (www.open-hospital.org)
+ * Copyright © 2006-2026 Informatici Senza Frontiere (info@informaticisenzafrontiere.org)
+ *
+ * Open Hospital is a free and open source software for healthcare data management.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * https://www.gnu.org/licenses/gpl-3.0-standalone.html
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package org.isf.country.service;
 
 import org.isf.country.model.Country;
@@ -10,12 +32,6 @@ import java.util.List;
 import java.util.Optional;
 
 public interface CountryIoOperationRepository extends JpaRepository<Country, Integer> {
-
-//	Optional<Country> findByIsoCodeAndActiveTrue(String isoCode);
-//
-//	List<Country> findAllByActiveTrueOrderByNameAsc();
-//
-//	Optional<Country> findByIdAndActiveTrue(int id);
 
 	Optional<Country> findByIsoCodeAndActive(String isoCode, int active);
 	List<Country> findAllByActiveOrderByNameAsc(int active);
