97a3adaba89288f3c0b6e64bb0677aaa9fb95b3e
serene21 <hemaserenankemenyi@gmail.com>
2026-05-13 17:10:43 +0100

    add reduction plan and price list to patient

--- sql/step_04_all_following_steps.sql
+++ sql/step_04_all_following_steps.sql
@@ -114,3 +114,5 @@ source step_a117_fix_ward_pharmacy.sql;
 source step_a118_add_radiology_permissions.sql;
 source step_a119_alter_table_examrow_and_laboratoryrow.sql;
 source step_a120_settings.sql;
+source step_a121_reduction_plan.sql;
+source step_a122_add_reduction_plan_and_price_list_to_patients.sql;
