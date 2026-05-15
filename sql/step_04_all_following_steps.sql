f32b18ed7cb2958b10c388c4df0f2e29cd858df6
JoelSpngho <songhojoel3@gmail.com>
2026-05-15 10:39:30 +0100

    git commit -m "Rename set and get 1.0 "

--- sql/step_04_all_following_steps.sql
+++ sql/step_04_all_following_steps.sql
@@ -110,8 +110,8 @@ source step_a113_alter_table_medicalinventory.sql;
 source step_a114_medical_type_soft_deletion.sql;
 source step_a115_add_missing_foreign_key_for_auditable_entities.sql;
 source step_a116_fix_alcohol_and_bowel_column.sql;
-source step_a117_fix_ward_pharmacy.sql;
+source step_ita117_fix_ward_pharmacy.sql;
 source step_a118_add_radiology_permissions.sql;
 source step_a119_alter_table_examrow_and_laboratoryrow.sql;
 source step_a120_settings.sql;
-source step_121_alter_oh_opd_add_referral_columns.sql
+source step_a121_add_refering_and_receiving_hospitals_in_table_opd.sql
