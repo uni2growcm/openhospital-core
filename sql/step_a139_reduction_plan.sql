ec91265ada83065184e94910030a781c7a35e78d
serene21 <hemaserenankemenyi@gmail.com>
2026-05-26 14:47:55 +0100

    rearrange scripts and remove gestational age from [pregnancy visits

--- sql/step_a139_reduction_plan.sql
+++ sql/step_a139_reduction_plan.sql
@@ -111,5 +111,17 @@ CREATE TABLE IF NOT EXISTS OH_OPERATIONSREDUCTION (
 )ENGINE = INNODB DEFAULT CHARACTER SET utf8;
 
 
-INSERT INTO `oh_menuitem` (`MNI_ID_A`, `MNI_BTN_LABEL`, `MNI_LABEL`, `MNI_TOOLTIP`, `MNI_SHORTCUT`, `MNI_SUBMENU`, `MNI_CLASS`, `MNI_IS_SUBMENU`, `MNI_POSITION`) VALUES ('reductionplan','angal.menu.btn.reductionplans','angal.menu.reductionplans','x','R','generaldata','org.isf.reductionplan.gui.ReductionPlanBrowser','N',13);
-INSERT INTO `oh_groupmenu` (`GM_ID`, `GM_UG_ID_A`, `GM_MNI_ID_A`, `GM_ACTIVE`, `GM_CREATED_BY`, `GM_CREATED_DATE`, `GM_LAST_MODIFIED_BY`, `GM_LAST_MODIFIED_DATE`) VALUES (352,'admin','reductionplan',1,NULL,NULL,NULL,NULL);
+INSERT IGNORE INTO oh_menuitem
+(MNI_ID_A, MNI_BTN_LABEL, MNI_LABEL, MNI_TOOLTIP, MNI_SHORTCUT,
+ MNI_SUBMENU, MNI_CLASS, MNI_IS_SUBMENU, MNI_POSITION)
+VALUES
+('reductionplan','angal.menu.btn.reductionplans',
+ 'angal.menu.reductionplans','x','R','generaldata',
+ 'org.isf.reductionplan.gui.ReductionPlanBrowser','N',13);
+
+ INSERT IGNORE INTO oh_groupmenu
+ (GM_ID, GM_UG_ID_A, GM_MNI_ID_A, GM_ACTIVE,
+  GM_CREATED_BY, GM_CREATED_DATE,
+  GM_LAST_MODIFIED_BY, GM_LAST_MODIFIED_DATE)
+ VALUES
+ (352,'admin','reductionplan',1,NULL,NULL,NULL,NULL);
--- src/main/java/org/isf/maternity/model/PregnancyVisit.java
+++ src/main/java/org/isf/maternity/model/PregnancyVisit.java
@@ -72,14 +72,6 @@ public class PregnancyVisit extends Auditable<String> {
 	private LocalDateTime visitDate;
 
 	@Nullable
-	@Column(name = "PRGV_GESTATIONAL_WEEKS")
-	private Integer gestationalWeeks;
-
-	@Nullable
-	@Column(name = "PRGV_GESTATIONAL_DAYS")
-	private Integer gestationalDays;
-
-	@Nullable
 	@Column(name = "PRGV_MATERNAL_WEIGHT")
 	private Double maternalWeight; // kg
 
@@ -158,6 +150,18 @@ public class PregnancyVisit extends Auditable<String> {
 	@Transient
 	private volatile int hashCode;
 
+	@Transient
+	public String getGestationalAge() {
+
+		if (pregnancy == null || visitDate == null) {
+			return null;
+		}
+
+		return pregnancy.getGestationalAge(
+			visitDate.toLocalDate()
+		);
+	}
+
 	public PregnancyVisit() {
 	}
 
@@ -189,22 +193,6 @@ public class PregnancyVisit extends Auditable<String> {
 
 	public void setVisitDate(LocalDateTime visitDate) {
 		this.visitDate = visitDate;
-	}
-
-	public Integer getGestationalWeeks() {
-		return gestationalWeeks;
-	}
-
-	public void setGestationalWeeks(Integer gestationalWeeks) {
-		this.gestationalWeeks = gestationalWeeks;
-	}
-
-	public Integer getGestationalDays() {
-		return gestationalDays;
-	}
-
-	public void setGestationalDays(Integer gestationalDays) {
-		this.gestationalDays = gestationalDays;
 	}
 
 	public Double getMaternalWeight() {
@@ -391,7 +379,7 @@ public class PregnancyVisit extends Auditable<String> {
 				"ID=" + id +
 			", pregnancyId=" + (pregnancy != null ? pregnancy.getId() : null) +
 				", visitDate=" + visitDate +
-				", gestationalWeeks=" + gestationalWeeks +
+				", gestationalWeeks=" + getGestationalAge() +
 				'}';
 	}
 }
