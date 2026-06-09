951e9ad5f182dec6238894cc1d4cdaa4cf456a13
rivaldou2g <nintedem.rivaldo@uni2growcameroun.com>
2026-06-09 15:10:04 +0100

    remove space

--- src/main/java/org/isf/patient/model/Patient.java
+++ src/main/java/org/isf/patient/model/Patient.java
@@ -134,7 +134,7 @@ public class Patient extends Auditable<String> {
 	private String transportMeans;
 
 	@Column(name="PAT_BLAMA")
-	private String blama; // name or number
+	private String blama;
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "PAT_COUNTRY_ID")
