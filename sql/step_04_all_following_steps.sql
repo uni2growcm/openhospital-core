a92fada9e5c0617f18c2594a08b306cf0a740b95
rivaldou2g <nintedem.rivaldo@uni2growcameroun.com>
2026-06-11 17:20:51 +0100

    refactor sql file

--- sql/step_04_all_following_steps.sql
+++ sql/step_04_all_following_steps.sql
@@ -150,4 +150,3 @@ source step_a153_create_index_in_medical_stock_ward.sql;
 source step_a154_add_maternity_submenuitem_and_privilege.sql;
 source step_a155_add_bill_refund_menuitem.sql;
 source step_a156_add_item_payments_table.sql;
-source step_a157_alter_table_patient_add_field.sql;
