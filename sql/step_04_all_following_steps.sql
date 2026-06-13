30bf66296de7e993cea3aefcc39912ee3dd8a250
rivaldou2g <nintedem.rivaldo@uni2growcameroun.com>
2026-06-13 13:17:52 +0100

    fix sql file

--- sql/step_04_all_following_steps.sql
+++ sql/step_04_all_following_steps.sql
@@ -153,6 +153,4 @@ source step_a156_add_item_payments_table.sql;
 source step_a157_maternity_pregnancy_obstetric_history.sql;
 source step_a158_add_home_visit_feature.sql;
 source step_a159_add_partners_feature.sql;
-source step_a157_statistics_submenus.sql;
-source step_a157_add_partners_feature.sql;
-source step_a158_maternity_pregnancy_obstetric_history.sql;
+source step_a160_statistics_submenus.sql;
