7737b774f36558b4cb217161e7dd2e06f9aebd16
rivaldou2g <nintedem.rivaldo@uni2growcameroun.com>
2026-06-16 11:54:31 +0100

    remove @transactionnal in staff repository

--- src/main/java/org/isf/homevisit/service/StaffIoOperationRepository.java
+++ src/main/java/org/isf/homevisit/service/StaffIoOperationRepository.java
@@ -27,7 +27,6 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -47,7 +46,6 @@ public interface StaffIoOperationRepository extends JpaRepository<Staff, Integer
 	List<Staff> searchActive(@Param("keyword") String keyword);
 
 	@Modifying
-	@Transactional
 	@Query("UPDATE Staff s SET s.active = 0 WHERE s.code = :code")
 	void softDelete(@Param("code") Integer code);
 }
