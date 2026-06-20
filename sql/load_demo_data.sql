3f70a10c1f384c3e11da1fd7d350ededd33c9e00
Joelu2g <songho.joel@uni2growcameroun.com>
2026-06-20 11:10:02 +0100

    script 160

--- sql/load_demo_data.sql
+++ sql/load_demo_data.sql
@@ -1,4 +1,4 @@
-/*M!999999\- enable the sandbox mode */ 
+-- M!999999- enable the sandbox mode */
 -- MariaDB dump 10.19  Distrib 10.6.23-MariaDB, for linux-systemd (x86_64)
 --
 -- Host: localhost    Database: ohdemo
