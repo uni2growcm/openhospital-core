dcd8c4de91bc7adc35d2cde29f7e1ff641edb755
Joelu2g <songho.joel@uni2growcameroun.com>
2026-06-22 17:30:34 +0100

    restore import

--- src/main/java/org/isf/opd/model/Opd.java
+++ src/main/java/org/isf/opd/model/Opd.java
@@ -23,6 +23,18 @@ package org.isf.opd.model;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+import jakarta.persistence.Version;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 
