24561212731c15f492fa6a2dd30efb54e5d9fbd7
Joelu2g <songho.joel@uni2growcameroun.com>
2026-06-27 08:13:12 +0100

    update step 4

--- sql/step_04_all_following_steps.sql
+++ sql/step_04_all_following_steps.sql
@@ -152,5 +152,4 @@ source step_a162_add_article_families_feature.sql;
 Source step_a164_add_submenu_in_matenity.sql;
 source step_a165_hiv_module.sql;
 source step_a166_familyplanning.sql;
-source step_a167_more_diagnosis_opd.sql;
-source step_a168_alter_table_exam_med_operation_add_articlefamiliy.sql;
+source step_a167_more_diagnoses_opd.sql;
