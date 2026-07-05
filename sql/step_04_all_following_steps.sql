0e162d794d85ed0ac56c29957b3369f97e07cc64
Aurorj <aurellejoellen@gmail.com>
2026-07-05 16:19:52 +0100

    align script

--- sql/step_04_all_following_steps.sql
+++ sql/step_04_all_following_steps.sql
@@ -151,6 +151,6 @@ source step_a161_add_admission_diagnosis_out_and_in_table.sql;
 source step_a162_add_article_families_feature.sql;
 Source step_a164_add_submenu_in_matenity.sql;
 source step_a165_hiv_module.sql;
-source step_a166_more_diagnosis_opd.sql;
+source step_a167_more_diagnosis_opd.sql;
 source step_a172_tuberculosis_module.sql;
 source step_a173_tuberculosis_menu.sql;
