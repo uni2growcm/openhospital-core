ac58252975bf8e55f17b0717d428a84766891f1b
duval2dev <duval_donfack@uni2growcameroun.com>
2026-08-11 11:22:31 +0100

    Add ENABLEBILLITEMGROUP parameter to hide item group in new bill

--- src/main/java/org/isf/generaldata/GeneralData.java
+++ src/main/java/org/isf/generaldata/GeneralData.java
@@ -40,6 +40,7 @@ public final class GeneralData extends ConfigurationProperties {
 	public static int PAGINATIONPAGESIZE;
 	public static boolean ENABLEMEDICALREFUND;
 	public static boolean ALLOWBILLGUARANTOR;
+	public static boolean ENABLEBILLITEMGROUP;
 	public static boolean ALLOWBILLPARTNER;
 	public static String LANGUAGE;
 	public static boolean HIGHLIGHTEXPIRINGMEDICAL;
@@ -123,6 +124,7 @@ public final class GeneralData extends ConfigurationProperties {
 	public static final int DEFAULT_PAGINATIONPAGESIZE = 100;
 	public static final boolean DEFAULT_STOCKMVTONBILLSAVE = false;
 	private static final boolean DEFAULT_ALLOWBILLGUARANTOR  = false;
+	private static final boolean DEFAULT_ENABLEBILLITEMGROUP  = false;
 	private static final boolean DEFAULT_ALLOWBILLPARTNER  = false;
 	private static final String DEFAULT_LANGUAGE = "en";
 	private static final boolean DEFAULT_SINGLEUSER = false;
@@ -218,6 +220,7 @@ public final class GeneralData extends ConfigurationProperties {
 		STOCKMVTONBILLSAVE = myGetProperty("STOCKMVTONBILLSAVE", DEFAULT_STOCKMVTONBILLSAVE);
 		PAGINATIONPAGESIZE = myGetProperty("PAGINATIONPAGESIZ", DEFAULT_PAGINATIONPAGESIZE);
 		ALLOWBILLGUARANTOR = myGetProperty("ALLOWBILLGUARANTOR", DEFAULT_ALLOWBILLGUARANTOR);
+		ENABLEBILLITEMGROUP = myGetProperty("ENABLEBILLITEMGROUP", DEFAULT_ENABLEBILLITEMGROUP);
 		ALLOWBILLPARTNER   = myGetProperty("ALLOWBILLPARTNER", DEFAULT_ALLOWBILLPARTNER);
 		ENABLEMEDICALREFUND = myGetProperty("ENABLEMEDICALREFUND", DEFAULT_ENABLEMEDICALREFUND);
 		SINGLEUSER = myGetProperty("SINGLEUSER", DEFAULT_SINGLEUSER);
