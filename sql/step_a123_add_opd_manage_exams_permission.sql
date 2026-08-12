f58ff8e7bb8c3a3d21bc367dddb6162834751e83
duval2dev <duval_donfack@uni2growcameroun.com>
2026-08-12 15:26:45 +0100

    (OH541) Add opd therapy button right

--- sql/step_a123_add_opd_manage_exams_permission.sql
+++ sql/step_a123_add_opd_manage_exams_permission.sql
@@ -10,3 +10,15 @@ INSERT INTO OH_GROUPMENU (GM_UG_ID_A, GM_MNI_ID_A, GM_ACTIVE)
 VALUES ('admin', 'opdexam', 1),
        ('doctor', 'opdexam', 1),
        ('laboratorist', 'opdexam', 1);
+
+--
+-- Add the "Therapy" button permission to OpdBrowser, granted by default to admin and doctor,
+-- matching the existing btnadmtherapy grant on AdmittedPatientBrowser.
+--
+
+INSERT INTO OH_MENUITEM (MNI_ID_A, MNI_BTN_LABEL, MNI_LABEL, MNI_TOOLTIP, MNI_SHORTCUT, MNI_SUBMENU, MNI_CLASS, MNI_IS_SUBMENU, MNI_POSITION)
+VALUES ('btnopdnewtherapy', 'angal.admission.therapy.btn', 'angal.admission.therapy.btn', 'x', 'T', 'opd', 'none', 'N', 4);
+
+INSERT INTO OH_GROUPMENU (GM_UG_ID_A, GM_MNI_ID_A, GM_ACTIVE)
+VALUES ('admin', 'btnopdnewtherapy', 1),
+       ('doctor', 'btnopdnewtherapy', 1);
